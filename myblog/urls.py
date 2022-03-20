95770991149fd9d05625dff532cdf347b8e19e66
BlackPrince163 <4523120100Ni>
2022-03-20 20:48:17 +0300

    + search page

--- myblog/urls.py
+++ myblog/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from myblog.views import MainView, PostDetailView, SignUpView, SignInView, FeedBackView, SuccessView
+from myblog.views import MainView, PostDetailView, SignUpView, SignInView, FeedBackView, SuccessView, SearchResultsView
 
 urlpatterns = [
     path('', MainView.as_view(), name="index"),
@@ -28,4 +28,5 @@ urlpatterns = [
     path('signout/', LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='signout'),
     path('contact', FeedBackView.as_view(), name='contact'),
     path('contact/success/', SuccessView.as_view(), name='success'),
+    path('search/', SearchResultsView.as_view(), name='search_results')
 ]
